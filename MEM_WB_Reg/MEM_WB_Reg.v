1dfcd8466fb9a636f42b87e28eb3abe4f09c8bfb
Karim Yehia <karimyehia15@icloud.com>
2025-12-01 22:28:17 +0200

    Create MEM_WB_Reg.v

---
+++ MEM_WB_Reg/MEM_WB_Reg.v
@@ -0,0 +1,21 @@
+module MEM_WB_Reg(
+    input clk, rst,
+    input wr_en,
+    input flush,
+    input mem_data_in,
+    output reg mem_data_out
+);
+
+always @(posedge clk or negedge rst) begin
+    if(!rst) begin
+        mem_data_out <= 0;
+    end
+    else if(flush) begin
+        mem_data_out <= 0;
+    end
+    else if(wr_en) begin
+        mem_data_out <= mem_data_in;
+    end
+end
+    
+endmodule
