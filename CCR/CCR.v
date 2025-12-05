4e46d5c782c100e306f7e119797c4fc7f96c45b5
Karim Yehia <karimyehia15@icloud.com>
2025-12-05 22:01:38 +0200

    Update CCR.v

--- CCR/CCR.v
+++ CCR/CCR.v
@@ -2,6 +2,7 @@ module CCR(
     input clk, rst,
     input Z, N, C, V,           // Flag inputs from ALU
     input flag_en,              // Enable flag updates
+    input [3:0] flag_mask,
     output reg [3:0] CCR_reg
 );
     
@@ -10,10 +11,18 @@ module CCR(
             CCR_reg <= 4'b0000;
         end
         else if(flag_en) begin
-            CCR_reg[0] <= Z;  // Zero flag
-            CCR_reg[1] <= N;  // Negative flag
-            CCR_reg[2] <= C;  // Carry flag
-            CCR_reg[3] <= V;  // Overflow flag
+            if(flag_mask[0]) begin
+                CCR_reg[0] <= Z;
+            end 
+            if(flag_mask[1]) begin 
+                CCR_reg[1] <= N;  
+            end
+            if(flag_mask[2]) begin
+                CCR_reg[2] <= C;  
+            end
+            if(flag_mask[3]) begin
+                CCR_reg[3] <= V;  
+            end
         end
     end
 endmodule
