4933aab4f14d67ea43b4a9e4fdb4d918c96e676f
Karim Yehia <karimyehia15@icloud.com>
2025-12-12 13:33:11 +0200

    Updated memory, registers and hu

--- IF_ID_Reg/IF_ID_reg.v
+++ IF_ID_Reg/IF_ID_reg.v
@@ -1,40 +1,54 @@
-module IF_ID_reg (
-    input wire              clk,
-    input wire              rst,
-    input wire              if_id_en,
-    input wire              flush,
-    input wire      [7:0]   pc_plus1,
-    input wire      [7:0]   instruction,
-    input wire      [7:0]   IP,
-    input wire      [7:0]   data_B,
-    output reg      [7:0]   pc_plus1_out,
-    output reg      [7:0]   instr_out,
-    output reg      [7:0]   IP_out,
-    output reg      [7:0]   data_B_out
+module IF_ID_Reg (
+    input wire       clk,
+    input wire       rst,
+    
+    // CONTROL INPUTS
+    input wire       IF_ID_EN, // Active Low Enable (0 = IF_ID_EN)
+    input wire       Flush,  // Active High Reset (1 = Clear)
+    
+    // DATA INPUTS
+    input wire [7:0] PC_Plus_1_In,
+    input wire [7:0] Instruction_In,
+    input wire [7:0] immby,
+    input wire [7:0] IP,
+    
+    // DATA OUTPUTS
+    output reg [7:0] PC_Plus_1_Out,
+    output reg [7:0] Instruction_Out,
+    output reg [7:0] immbyout,
+    output reg [7:0] IP_out
 );
 
-always @(posedge clk or negedge rst) 
-begin
-    if(!rst)
-    begin
-        pc_plus1_out  <= 0; // pc = 0, pc+1 = 1
-        instr_out <= 8'd0;
-        IP_out <= 8'd0;
-        data_B_out <= 8'd0;
+    always @(posedge clk or negedge rst) begin
+        if (!rst) 
+        begin
+            // Asynchronous Reset
+            Instruction_Out <= 8'b00000000; // NOP
+            PC_Plus_1_Out   <= 8'b00000000;
+            immbyout        <='b0;
+            IP_out          <= 0;
+        end 
+        else if (Flush) begin
+            // Synchronous Flush (Branch Taken)
+            Instruction_Out <= 8'b00000000; // Force NOP
+            // PC value doesn't matter during flush, but clearing is safe
+            PC_Plus_1_Out   <= 8'b00000000; 
+            immbyout        <='b0;
+            IP_out          <= 0;
+        end 
+        else if (IF_ID_EN) 
+        begin 
+            // Normal Operation (IF_ID_EN=1 means Enable)
+            // Note: If logic says IF_ID_EN=0 means Stall, change this condition.
+            // Usually: if (Write_En) -> Update.
+            
+            Instruction_Out <= Instruction_In;
+            PC_Plus_1_Out   <= PC_Plus_1_In;
+            IP_out <= IP;
+        end
+        else
+        immbyout        <=immby;
+        
     end
-    else if(flush) //NOP
-    begin
-        instr_out <= 8'd0; // opcode = 0
-        pc_plus1_out <= pc_plus1; // pc+1 -> pc
-        // rest of outputs should stay the same as NO_OP will not use any of them.
-    end
-    else if(if_id_en)
-    begin
-        pc_plus1_out <= pc_plus1;
-        instr_out <= instruction;
-        IP_out <= IP;
-        data_B_out <= data_B;
-    end
-end
-    
+
 endmodule
