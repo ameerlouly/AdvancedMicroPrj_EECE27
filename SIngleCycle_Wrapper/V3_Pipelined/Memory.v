8a2401e7827be2b66cb24595eadbfb1edb253564
farhan036 <112325222+farhan036@users.noreply.github.com>
2025-12-14 23:50:44 +0200

    Update Memory.v

--- SIngleCycle_Wrapper/V3_Pipelined/Memory.v
+++ SIngleCycle_Wrapper/V3_Pipelined/Memory.v
@@ -4,11 +4,11 @@ module memory (
 
     /**** Port A Instruction Fetch *****/
     input  wire [7:0]  addr_a,
-    output reg  [7:0]  data_out_a,
+    output wire  [7:0]  data_out_a,
 
     ///// Port B Data Memory /////
     input  wire [7:0]  addr_b,
-    output reg  [7:0]  data_out_b,
+    output wire  [7:0]  data_out_b,
     input  wire        we_b,             // write enable for port B
     input  wire [7:0]  write_data_b
 );
@@ -20,36 +20,27 @@ module memory (
     // initial begin
     //     $readmemh("program.hex", mem); // create program.hex for sim
     // end
-
+integer i ;
     // Synchronous behavior: read/write on posedge clk
     always @(posedge clk or negedge rst) 
     begin
         if (!rst) 
         begin
-            
-            data_out_a  <= 8'd0;
-            data_out_b <= 8'd0;
-            
+            for (i=0; i<=255;i=i+1)
+                begin
+                    mem[i] <= 'd0;
+                end
         end
         else 
         begin
-            // If a write to addr_b happens this cycle, we want deterministic behavior:
-            // - data_out_b should return the newly written value (read-after-write).
-            // - data_out_a should reflect the new value if fetch address equals addr_b (addr_a == addr_b).
             if (we_b) 
             begin
                 mem[addr_b] <= write_data_b;   // perform write
-                data_out_b  <= write_data_b;   // read-after-write deterministic
                 // instruction fetch sees the new data if it's the same address
-                if (addr_a == addr_b)
-                    data_out_a <= write_data_b;
-                else
-                    data_out_a <= mem[addr_a];
             end
 
-            data_out_b <= mem[addr_b];
-            data_out_a  <= mem[addr_a];
         end
     end
-
+assign data_out_b  = mem[addr_b];
+assign data_out_a  = mem[addr_a];
 endmodule
